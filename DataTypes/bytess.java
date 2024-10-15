557ca4d6209db843413de323977b7ccab2f7974f
bshadabkhan <shadabkhanbenki@gmail.com>
2024-10-15 19:01:33 +0530

    Java Basics

---
+++ DataTypes/bytess.java
@@ -0,0 +1,19 @@
+package DataTypes;
+
+public class bytess {
+    public static void main(String[] args) {
+        // -128 to 127 // 8 bits
+        byte number = 100;
+        short num = 100;
+        int num1 = 100;
+        float nums = 157.87f;
+        long num2 = 8000;
+        double num3 = 1545.45;
+        System.out.println("Value of num3: " + num3);
+        System.out.println("Value of nums: " + nums);
+        System.out.println("Value of num2: " + num2);
+        System.out.println("Value of num1: " + num1);
+        System.out.println("value of num: " + num);
+        System.out.println("Value of num1: " + number);
+    }
+}
--- app.java
+++ app.java
@@ -1,6 +1,13 @@
 public class app {
     public static void main(String[] args) {
-
+        // -(Single line comments) the following line prints Hello,World!
+        /*
+         * multi line comments
+         * sddsd
+         * dsdsd
+         * dsdsdd
+         * dsdaawd
+         */
         System.out.println("Hello, World!");
 
     }
