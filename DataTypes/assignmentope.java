f259f47c8039ec732fa69a278786541f892662e5
bshadabkhan <shadabkhanbenki@gmail.com>
2024-10-17 01:00:47 +0530

    Java Basics

---
+++ DataTypes/assignmentope.java
@@ -0,0 +1,15 @@
+package DataTypes;
+
+public class assignmentope {
+    // +=,-=,*=,/=,%=
+    public static void main(String[] args) {
+
+        int num1 = 3;
+        int num2 = 5;
+        int result = 7;
+        result %= num1 + num2;
+        System.out.println("result: " + result);
+
+    }
+
+}
