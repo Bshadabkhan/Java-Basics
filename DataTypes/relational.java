699172121ef26f10026069fb4a84b7522892078d
bshadabkhan <shadabkhanbenki@gmail.com>
2024-10-17 00:46:54 +0530

    Java Basics

---
+++ DataTypes/relational.java
@@ -0,0 +1,13 @@
+package DataTypes;
+
+public class relational {
+    // ==,!=,>,<,>=,<=
+    public static void main(String[] args) {
+        int num1 = 6;
+        int num2 = 7;
+        boolean result = num1 == num2;
+
+        System.out.println("result: " + result);
+
+    }
+}
