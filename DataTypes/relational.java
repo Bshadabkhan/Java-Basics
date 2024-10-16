baaf9c3ca62cd36e7d0781bcd8428d953d68d873
bshadabkhan <shadabkhanbenki@gmail.com>
2024-10-17 00:47:51 +0530

    Java Basics

--- DataTypes/relational.java
+++ DataTypes/relational.java
@@ -5,7 +5,7 @@ public class relational {
     public static void main(String[] args) {
         int num1 = 6;
         int num2 = 7;
-        boolean result = num1 == num2;
+        boolean result = num1 >= num2;
 
         System.out.println("result: " + result);
 
