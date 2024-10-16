eb9c11360c11c51c57d0d7f54ab6825ac71f519d
bshadabkhan <shadabkhanbenki@gmail.com>
2024-10-16 09:02:27 +0530

    Java Basics

---
+++ DataTypes/arithoperators.java
@@ -0,0 +1,14 @@
+package DataTypes;
+
+public class arithoperators {
+    public static void main(String[] args) {
+        // -,+,/,*,%
+
+        double num1 = 5;
+        double num2 = 7;
+        double num3 = num1 / num2;
+        System.out.println(num3);
+
+    }
+
+}
