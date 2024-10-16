c2058e2f9de7bd53771bfa9c85bc20710dc29458
bshadabkhan <shadabkhanbenki@gmail.com>
2024-10-17 00:54:57 +0530

    Java Basics

---
+++ DataTypes/logicalope.java
@@ -0,0 +1,15 @@
+package DataTypes;
+
+public class logicalope {
+    public static void main(String[] args) {
+
+        boolean first = false;
+        boolean second = true;
+        boolean result = first && second;
+        boolean result1 = first || second;
+        System.out.println("result1" + result1);
+        System.out.println("result: " + result);
+
+    }
+
+}
