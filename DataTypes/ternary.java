93c80ad056fe1c4c42eb957ee912893463ac5e30
bshadabkhan <shadabkhanbenki@gmail.com>
2024-10-17 01:06:50 +0530

    Java Basics

---
+++ DataTypes/ternary.java
@@ -0,0 +1,12 @@
+package DataTypes;
+
+public class ternary {
+    public static void main(String[] args) {
+
+        int num1 = 2;
+        int num2 = 3;
+        int num3 = num1 > num2 ? num1 : num2;
+        System.out.println(num3);
+
+    }
+}
